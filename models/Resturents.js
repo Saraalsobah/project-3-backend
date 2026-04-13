03d48fe850f9c013fbf6e8b7812785870e9e7cf1
noor <nooralalaiwat@gmail.com>
2026-04-13 20:20:43 +0300

    restaurant + menu model

---
+++ models/Resturents.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const resturentsSchema = new mongoose.Schema({
+    name: {type:String, required:true},
+    location: {type:String, required:true},
+    cuisine: {type:String, required:true},
+    logourl: {type:String, required:true},
+    menu: [{type:mongoose.Schema.Types.ObjectId, ref:'Menu'}],
+    author: {type:mongoose.Schema.Types.ObjectId, ref:'User'},
+    reviews: [{type:mongoose.Schema.Types.ObjectId, ref:'Review'}]
+}, {timestamps:true})
+
+const menuitemSchema = new mongoose.Schema(
+    {
+
+        name:{
+            type:String ,
+            required:true,
+            trim:true,
+        },
+
+        price:{
+            type:Number ,
+            required:true,
+            min:0,
+        },
+
+        category:{
+            type:String ,
+            enum:['Appetizer','Main','Dessert','Drink','Side'],
+            required:true,
+        },
+
+    },
+    
+    {timestamps:true}
+);
+module.exports = mongoose.model('Menuitem', menuitemSchema);
+module.exports = Menuitem
+const Resturents = mongoose.model('Resturents', resturentsSchema)
+module.exports = Resturents
