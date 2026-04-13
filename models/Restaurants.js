82b3f902a18dff241ac3f80deabc49c6f28b7555
noor <nooralalaiwat@gmail.com>
2026-04-13 20:10:00 +0300

    added restaurant model

--- models/Restaurants.js
+++ models/Restaurants.js
@@ -10,8 +10,32 @@ const resturentsSchema = new mongoose.Schema({
     reviews: [{type:mongoose.Schema.Types.ObjectId, ref:'Review'}]
 }, {timestamps:true})
 
+const menuitemSchema = new mongoose.Schema(
+    {
 
+        name:{
+            type:String ,
+            required:true,
+            trim:true,
+        },
 
+        price:{
+            type:Number ,
+            required:true,
+            min:0,
+        },
+
+        category:{
+            type:String ,
+            enum:['Appetizer','Main','Dessert','Drink','Side'],
+            required:true,
+        },
+
+    },
+    
+    {timestamps:true}
+);
+module.exports = mongoose.model('Menuitem', menuitemSchema);
 
 const Resturents = mongoose.model('Resturents', resturentsSchema)
 module.exports = Resturents
